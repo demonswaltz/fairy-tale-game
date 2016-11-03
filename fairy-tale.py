3d89d8e14410ee9ca51575f898269ac7ae61b75c
Jessica Duffy <jduffy@apld.local>
2016-11-03 12:47:38 -0500

    change resolution

--- fairy-tale.py
+++ fairy-tale.py
@@ -1,7 +1,24 @@
 import pygame
 
 pygame.init()
-width, height = 640, 480
+width, height = 1000,750
 screen=pygame.display.set_mode((width, height))
 keys = [False, False, False, False]
 playerpos=[100,100]
+level=1
+
+#images
+vertline = pygame.image.load("resources/images/vert-line.png")
+
+#Game Layout
+while 1:
+    screen.fill(1000)
+    screen.blit(vertline,(500,1000))
+
+
+
+#level 1
+
+
+
+
