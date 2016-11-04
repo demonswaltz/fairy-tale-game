f2f0ff36234d32431fc1026d98fd14aff842b193
jess <jessica.l.duffy@gmail.com>
2016-11-03 19:31:27 -0500

    add backgroun and grid

--- fairy-tale.py
+++ fairy-tale.py
@@ -12,9 +12,17 @@ vertline = pygame.image.load("resources/images/vert-line.png")
 
 #Game Layout
 while 1:
-    screen.fill(1000)
-    screen.blit(vertline,(500,1000))
-
+    screen.fill((0,110,0))
+    screen.blit(vertline,(650,-55))
+# 7 - update the screen
+    pygame.display.flip()
+    # 8 - loop through the events
+    for event in pygame.event.get():
+        # check if the event is the X button 
+        if event.type==pygame.QUIT:
+            # if it is quit the game
+            pygame.quit() 
+            exit(0)
 
 
 #level 1
