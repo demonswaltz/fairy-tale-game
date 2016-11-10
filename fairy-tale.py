7070e4d37d7b0b8224682e297776a4213620bb69
jess <jessica.l.duffy@gmail.com>
2016-11-10 08:53:50 -0600

    print arrows, kind of

--- fairy-tale.py
+++ fairy-tale.py
@@ -32,19 +32,25 @@ def gameSetup(str):
 		pygame.display.flip()
 #Draw Moves Function
 def drawMoves():
+	arrowPosy=[16, 66, 116, 166, 216, 266, 316, 366, 416, 466, 516, 566, 616, 666];
+	imageLoad=[]
 	global moveImg
-	for moves in moveImg
+	global moves
+	if len(moves) > 0:
+		imageLoad.append(pygame.image.load(moveImg[len(moves)-1]))
+		for image in imageLoad:
+			for pos in arrowPosy:
+				screen.blit(image, (750, arrowPosy[len(moves)-1]))
+				pygame.display.flip()
+		
 		
 #Basic Game Mechanics
 def gamePlay():
 	global index 
-	global moves 
-	arrowPosy=[16, 66, 116, 166, 216, 266, 316, 366, 416, 466, 516, 566, 616, 666];
+	global moves
+	arrowPosy=[16, 66, 116, 166, 216, 266, 316, 366, 416, 466, 516, 566, 616, 666]; 
 	spaceBar=False
-	arrowBlitPosx= 750
-	while spaceBar == False and index <= 14:
-		print index
-		print spaceBar
+	while spaceBar == False and index < 14:
 		for event in pygame.event.get():
 			if event.type == pygame.KEYDOWN:
 				if event.key == pygame.K_UP:
@@ -69,7 +75,8 @@ def gamePlay():
 					index-= 1
 				elif event.key == pygame.K_SPACE:
 					spaceBar = True
-				
+		drawMoves()
+			
 		
 	else:
 		global count
@@ -95,9 +102,12 @@ if level == 1:
 	for x in range(width/grass.get_width()+1):
 		for y in range(height/grass.get_height()+1):
 			puzzleArea.blit(grass,(x*125,y*200))
-	gameSetup("LEVEL 1")
-	#gamePlay()
-
+	while count < 1:
+		gameSetup("LEVEL 1")
+		gamePlay()
+	else:
+		while True:
+			gameSetup("LEVEL 1")
 #level 2
 elif level == 2:
 	for x in range(width/grass.get_width()+1):
@@ -106,9 +116,11 @@ elif level == 2:
 	while count < 1:
 		gameSetup("LEVEL 2")
 		gamePlay()
+		drawMoves()
 	else:
 		while True:
 			gameSetup("LEVEL 2")
+			drawMoves()
 # 7 - update the screen
 	
 # 8 - loop through the events
