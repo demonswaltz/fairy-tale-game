29f30924176409845e5d21be64bf485e9ff6da4c
jess <jessica.l.duffy@gmail.com>
2016-11-10 19:02:48 -0600

    break arrow printing differently

--- fairy-tale.py
+++ fairy-tale.py
@@ -12,6 +12,7 @@ level=2
 index = 0
 moves= [];
 moveImg=[];
+imageLoad=[];
 count = 0
 
 #images
@@ -37,54 +38,59 @@ def drawMoves():
 	global moveImg
 	global moves
 	if len(moves) > 0:
-		imageLoad.append(pygame.image.load(moveImg[len(moves)-1]))
+		imageLoad.append(screen.blit(pygame.image.load(moveImg[len(moveImg)-1]), (750, arrowPosy[len(moveImg)-1])))
 		for image in imageLoad:
-			for pos in arrowPosy:
-				screen.blit(image, (750, arrowPosy[len(moves)-1]))
-				pygame.display.flip()
-		
+			image
+			pygame.display.flip()
 		
 #Basic Game Mechanics
 def gamePlay():
 	global index 
 	global moves
-	arrowPosy=[16, 66, 116, 166, 216, 266, 316, 366, 416, 466, 516, 566, 616, 666]; 
 	spaceBar=False
 	while spaceBar == False and index < 14:
+		print len(moveImg) 
 		for event in pygame.event.get():
-			if event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_UP:
-					moves.append("up")
-					moveImg.append(up)
-					index += 1
-				elif event.key == pygame.K_DOWN:
-					moves.append("down")
-					moveImg.append(down)
-					index += 1
-				elif event.key == pygame.K_LEFT:
-					moves.append("left")
-					moveImg.append(left)
-					index += 1
-				elif event.key == pygame.K_RIGHT:
-					moves.append("right")
-					moveImg.append(right)
-					index += 1
-				elif event.key == pygame.K_BACKSPACE:
-					moves.pop()
-					moveImg.pop()
-					index-= 1
-				elif event.key == pygame.K_SPACE:
-					spaceBar = True
+			try:
+				if event.type == pygame.KEYDOWN:
+					gameSetup("Level")
+					drawMoves()
+					if event.key == pygame.K_UP:
+						moves.append(pygame.K_UP)
+						moveImg.append(up)
+						index += 1
+					elif event.key == pygame.K_DOWN:
+						moves.append(pygame.K_DOWN)
+						moveImg.append(down)
+						index += 1
+					elif event.key == pygame.K_LEFT:
+						moves.append(pygame.K_LEFT)
+						moveImg.append(left)
+						index += 1
+					elif event.key == pygame.K_RIGHT:
+						moves.append(pygame.K_RIGHT)
+						moveImg.append(right)
+						index += 1
+					elif event.key == pygame.K_BACKSPACE:
+						moves.pop()
+						moveImg.pop()
+						index-= 1
+					elif event.key == pygame.K_SPACE:
+						spaceBar = True
+			except IndexError:
+				index = 0
+				gameSetup("LEVEL")
+				drawMoves()
+				gamePlay()
 		drawMoves()
-			
+		
 		
 	else:
 		global count
-		if count < 1:
-			print moves
-			print moveImg
-			print count
-			count += 1
+		print moves
+		print index
+		print moveImg
+		count += 1
 		
 #for move in moveImg:
 		#	arrowPic=[];
@@ -116,11 +122,11 @@ elif level == 2:
 	while count < 1:
 		gameSetup("LEVEL 2")
 		gamePlay()
-		drawMoves()
+		
+		
 	else:
 		while True:
-			gameSetup("LEVEL 2")
-			drawMoves()
+			gamePlay()
 # 7 - update the screen
 	
 # 8 - loop through the events
